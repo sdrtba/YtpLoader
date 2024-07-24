afc780318eff58230f6616181ff253edc27bb85a
sdrtba <sdrtba@yandex.ru>
2024-07-24 14:07:25 +0400

    update

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import yt_dlp
 
 def download(url):
     ydl_opts = {
-        'format': 'ba[ext=m4a]', # bestaudio.m4a
+        'outtmpl': '%(title)s.%(ext)s', # output template to name files as <title>.<ext>
         'paths': {'home': 'output'}, # temp and main files in one folder
-        'retries': 10,
+        'format': 'ba[ext=m4a]', # bestaudio.m4a
+        'retries': 4,
         'quiet': True, # without logs
         'progress': True, # show progress bar
         'skip_unavailable_fragments': True,
+	'noincludeunavailablevideos': True,
+	'ignoreerrors': True,
+	'no_warnings': True,
+	'playliststart': 0,
     }
 
     try:
@@ -19,5 +24,5 @@ def download(url):
 
 if __name__ == '__main__':
     #url = input('link: ')
-    url = "https://www.youtube.com/playlist?list=PLcLWzrwuuZhNet5VdtPJBV-K0WDcRSvhJ"
+    url = "https://www.youtube.com/playlist?list=PLcLWzrwuuZhP-qE-ttdWn0x8ANgR8xzpC"
     download(url)
