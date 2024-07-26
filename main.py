359458e8eaae1eabbe60bf17c98166a0494a4e57
sdrtba <sdrtba@yandex.ru>
2024-07-26 19:34:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import yt_dlp
 
-def download(url):
+def download():
     ydl_opts = {
         'outtmpl': '%(title)s.%(ext)s', # output template to name files as <title>.<ext>
         'paths': {'home': 'output'}, # temp and main files in one folder
@@ -9,20 +9,24 @@ def download(url):
         'quiet': True, # without logs
         'progress': True, # show progress bar
         'skip_unavailable_fragments': True,
-	'noincludeunavailablevideos': True,
-	'ignoreerrors': True,
-	'no_warnings': True,
-	'playliststart': 0,
+	    'noincludeunavailablevideos': True,
+	    'ignoreerrors': True,
+	    'no_warnings': True,
+	    'playliststart': skip,
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download(url)
-    except:
-        print('Oops...')
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
-    #url = input('link: ')
-    url = "https://www.youtube.com/playlist?list=PLcLWzrwuuZhP-qE-ttdWn0x8ANgR8xzpC"
-    download(url)
+    url = input('link (pass for https://www.youtube.com/playlist?list=PLcLWzrwuuZhP-qE-ttdWn0x8ANgR8xzpC): ')
+    url = url if url != '' else "https://www.youtube.com/playlist?list=PLcLWzrwuuZhP-qE-ttdWn0x8ANgR8xzpC"
+
+    skip = input('skip (pass for 0): ')
+    skip = skip if skip != '' else 3
+
+    download()
